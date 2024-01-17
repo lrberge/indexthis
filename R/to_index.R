dcf7c17726fbb14078cefadff6c0260bbdebdb73
laurent berge <laurent.berge@hotmail.fr>
2024-01-17 11:50:17 +0100

    add doc to to_index

--- R/to_index.R
+++ R/to_index.R
@@ -5,11 +5,78 @@
 #------------------------------------------------------------------------------#
 
 
-
-to_index = function(..., list = NULL, sorted = FALSE, add_items = FALSE, out.list = FALSE,
+#' Turns one or multiple vectors into an index (aka group id, aka key)
+#' 
+#' Turns one or multiple vectors of the same length into an index, that is a integer vector 
+#' of the same length ranging from 1 to the number of unique elements in the vectors. 
+#' This is equivalent to creating a key.
+#' 
+#' @param ... The vectors to be turned into indexes. Only works for atomic vectors. 
+#' If multiple vectors are provided, they should all be of the same length. Notes that 
+#' you can alternatively provide a list of vectors with the argument `list`.
+#' @param list An alternative to using `...` to pass the input vectors. If provided, it
+#' should be a list of atomic vectors, all of the same length. If this argument is provided,
+#' then `...` is ignored.
+#' @param sorted Logical, default is `FALSE`. By default the index order is based on 
+#' the order of occurence. Values occurring before have lower index values. Use `sorted=TRUE`
+#' to have the index to be sorted based on the vector values. For example `c(7, 3, 7, -8)` will be 
+#' turned into `c(1, 2, 1, 3)` if sorted=FALSE and into `c(3, 2, 3, 1)` is `sorted=TRUE`.
+#' @param items.out Logical, default is `FALSE`. Whether to return the input values the indexes
+#' refer to. If `TRUE`, the attribute `"items"` is created with the vector values. Note that
+#' you can return instead a list insteda of an attribute with `out.list = TRUE`.
+#' @param out.list Logical, default is `FALSE`. If `TRUE`, the function returns a list 
+#' of two elements: `index` and `items`. The `items` is the unique input elements 
+#' the index refers to.
+#' @param items.df Logical, default is `FALSE`. Whether to return the input elements (to which the index refer) in the form of a data.frame.
+#' @param items.join Character scalar, default is `"_"`. Only used if the items are returned and 
+#' there were multiple vectors as input and if `items.df=FALSE`. If there are multiple 
+#' vectors in input, their unique elements are joined with `items.join`, so that a single
+#' character vector represent their combination.
+#' @param internal Logical, default is `FALSE`. If `TRUE`, some checks on the data are ignored.
+#' 
+#' @author 
+#' Laurent Berge
+#' 
+#' 
+#' @examples
+#' 
+#' x = c("u", "a", "a", "s", "u", "u")
+#' y = c(5, 5, 5, 3, 3, 7)
+#' 
+#' # By default, the index value is based on order of occurrence
+#' to_index(x)
+#' to_index(y)
+#' to_index(x, y)
+#' 
+#' # Use the order of the input values with sorted=TRUE
+#' to_index(x, sorted = TRUE)
+#' to_index(y, sorted = TRUE)
+#' to_index(x, y, sorted = TRUE)
+#' 
+#' # To get the values to which the index refer, use items.out
+#' to_index(x, items.out = TRUE)
+#' 
+#' # play around with the format of the output
+#' to_index(x, items.out = TRUE, out.list = TRUE)
+#' to_index(x, items.out = TRUE, out.list = TRUE, items.df = TRUE)
+#' 
+#' # multiple items are by default coerced into a single character string ...
+#' to_index(x, y, items.out = TRUE)
+#' 
+#' # ... to avoid this, use items.df = TRUE
+#' to_index(x, y, items.out = TRUE, items.df = TRUE)
+#' to_index(x, y, items.out = TRUE, items.df = TRUE, sorted = TRUE)
+#' 
+#' # NAs considered as valid
+#' x_NA = c("u", NA, "a", "a", "s", "u", "u")
+#' to_index(x_NA, items.out = TRUE)
+#' to_index(x_NA, items.out = TRUE, sorted = TRUE)
+#' 
+#' 
+to_index = function(..., list = NULL, sorted = FALSE, items.out = FALSE, out.list = FALSE,
                     items.df = FALSE, items.join = "_", internal = FALSE){
 
-  check_arg(sorted, add_items, out.list, items.df, "logical scalar")
+  check_arg(sorted, items.out, out.list, items.df, "logical scalar")
   check_arg(items.join, "character scalar")
   
   check_arg(list, "NULL list")
@@ -36,7 +103,7 @@ to_index = function(..., list = NULL, sorted = FALSE, add_items = FALSE, out.lis
   
   info = to_index_internal(dots)
   index = info$index
-  if (sorted || add_items) {
+  if (sorted || items.out) {
     
     # vector of the first items
     items_unik = vector("list", Q)
@@ -75,13 +142,17 @@ to_index = function(..., list = NULL, sorted = FALSE, add_items = FALSE, out.lis
       row.names(items) = 1:nrow(items)
       
     } else {
-      # we "paste" them
-      arg_list = items_unik
-      arg_list$sep = items.join
-      items = do.call("paste", arg_list)
+      # we "paste" them if Q > 1
+      if(Q == 1){
+        items = items_unik
+      } else {
+        arg_list = items_unik
+        arg_list$sep = items.join
+        items = do.call("paste", arg_list)
+      }
     }
 
-    if(add_items){
+    if(items.out){
       res = list(index = index, items = items)
     } else {
       res = index
@@ -91,7 +162,7 @@ to_index = function(..., list = NULL, sorted = FALSE, add_items = FALSE, out.lis
     res = index
   }
 
-  if(add_items && isFALSE(out.list)){
+  if(items.out && isFALSE(out.list)){
     res_tmp = res$index
     attr(res_tmp, "items") = res$items
     res = res_tmp
