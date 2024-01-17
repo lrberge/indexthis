015fdcc3f94136440613c889e9783c0c3eabedd8
laurent berge <laurent.berge@hotmail.fr>
2024-01-17 14:19:55 +0100

    fix NA bug

--- src/to_index.cpp
+++ src/to_index.cpp
@@ -78,28 +78,40 @@ r_vector::r_vector(SEXP x){
       this->px_dbl = REAL(x);
       IS_INT = true;
       double *px = REAL(x);
-      double x_min = px[0], x_max = px[0], x_tmp;
-      bool any_na = false;
-      for(int i=1 ; i<n ; ++i){
-        x_tmp = px[i];
-        
-        if(std::isnan(x_tmp)){
-          any_na = true;
-        } else if(!(x_tmp == (int) x_tmp)){
-          IS_INT = false;
-          break;
-        } else if(x_tmp > x_max){
-          x_max = x_tmp;
-        } else if(x_tmp < x_min){
-          x_min = x_tmp;
-        }
+      double x_min = 0, x_max = 0, x_tmp;
+      
+      // taking care of NA corner cases
+      int i_start = 0;
+      while(i_start < n && std::isnan(px[i_start])){
+        ++i_start;
       }
+      
+      bool any_na = i_start > 0;
+      if(i_start < n){
+        x_min = px[i_start];
+        x_max = px[i_start];
+        
+        for(int i=i_start ; i<n ; ++i){
+          x_tmp = px[i];
+          
+          if(std::isnan(x_tmp)){
+            any_na = true;
+          } else if(!(x_tmp == (int) x_tmp)){
+            IS_INT = false;
+            break;
+          } else if(x_tmp > x_max){
+            x_max = x_tmp;
+          } else if(x_tmp < x_min){
+            x_min = x_tmp;
+          }
+        }
+      }      
       
       this->any_na = any_na;
 
       this->x_min = static_cast<int>(x_min);
       // +1 for the NAs
-      this->x_range = x_max - x_min + 1;
+      this->x_range = x_max - x_min + 2;
       
       type = IS_INT ? T_DBL_INT : T_DBL;
     } else {
@@ -110,27 +122,38 @@ r_vector::r_vector(SEXP x){
       
       if(TYPEOF(x) == INTSXP){
         int *px = INTEGER(x);
-        int x_min = px[0], x_max = px[0], x_tmp;
-        bool any_na = x_min == NA_INTEGER;
-        for(int i=1 ; i<n ; ++i){
-          x_tmp = px[i];
+        int x_min = 0, x_max = 0, x_tmp;
+        
+        // taking care of NA corner cases
+        int i_start = 0;
+        while(i_start < n && px[i_start] == NA_INTEGER){
+          ++i_start;
+        }
+        bool any_na = i_start > 0;
+        if(i_start < n){
+          x_min = px[i_start];
+          x_max = px[i_start];
           
-          if(x_tmp > x_max){
-            x_max = x_tmp;
-          } else if(x_tmp < x_min){
-            // NA integer is the smallest int, defined as -2147483648
-            if(x_tmp == NA_INTEGER){
-              any_na = true;
-            } else {
-              x_min = x_tmp;
-            }            
+          for(int i=i_start ; i<n ; ++i){
+            x_tmp = px[i];
+            
+            if(x_tmp > x_max){
+              x_max = x_tmp;
+            } else if(x_tmp < x_min){
+              // NA integer is the smallest int, defined as -2147483648
+              if(x_tmp == NA_INTEGER){
+                any_na = true;
+              } else {
+                x_min = x_tmp;
+              }            
+            }
           }
         }
         
         this->any_na = any_na;
         this->x_min = x_min;
         // +1 for the NAs
-        this->x_range = x_max - x_min + 1;
+        this->x_range = x_max - x_min + 2;
       } else if(TYPEOF(x) == LGLSXP){
         this->x_min = 0;
         // 0, 1, NA
@@ -177,7 +200,7 @@ r_vector::r_vector(SEXP x){
   if(IS_INT){
     // finding out if we're in the easy case
     this->x_range_bin = power_of_two(this->x_range);    
-    this->is_fast_int = this->x_range < 100000 || this->x_range_bin <= power_of_two(2.5*n);
+    this->is_fast_int = this->x_range < 100000 || this->x_range <= 2*n;
     this->NA_value = this->x_range - 1;
   }  
 }
--- tests/index_tests.R
+++ tests/index_tests.R
@@ -6,6 +6,55 @@
 
 
 
+n = 500
+
+words = paste0(rep(letters, 5), rep(letters, each = 5), rep(rev(letters), 5))
+
+years = 1800:2023
+months = 1:12
+day = 1:28
+date_sample = as.Date(paste0(sample(years, 200, TRUE), "-", 
+                             sample(months, 200, TRUE), "-", 
+                             sample(day, 200, TRUE)))
+
+
+set.seed(1)
+base = list(
+  int = as.integer(rnorm(n, sd = 8)),
+  fact = factor(sample(letters[-(1:5)], n, TRUE), letters),
+  bool = sample(c(TRUE, FALSE), n, TRUE),
+  dbl = round(rnorm(n, sd = 8), 1),
+  dbl_int = round(rnorm(n, sd = 8)),
+  char = sample(words, n, TRUE),
+  date = sample(date_sample, n, TRUE),
+  complex = complex(real = round(rnorm(n, sd = 4)), imaginary = round(rnorm(n, sd = 4)))
+)
+
+
+# single vector
+for(i_type in seq_along(base)){
+  cat(names(base)[i_type])
+  x = base[[i_type]]
+  for(any_na in c(FALSE, TRUE)){
+    cat(".")
+    if(any_na){
+      x[c(1, 32, 65, 125)] = NA
+    }
+    
+    index = to_index(x)
+    
+    x_char = as.character(x)
+    if(any_na){
+      x_char[is.na(x_char)] = "NA"
+    }
+    index_r = unclass(as.factor(x_char))
+    
+    test(nrow(unique(data.frame(index, index_r))), max(index))
+  }
+  cat("\n")
+}
+
+
 
 
 
